5c5aebc684610e27c80f26ade91cd38fbd7711e1
deviffy <deviffy@gmail.com>
2015-12-04 13:06:33 +0200

    Update Service Provider

--- src/SonusServiceProvider.php
+++ src/SonusServiceProvider.php
@@ -1,34 +1,39 @@
-<?php namespace Closca\Sonus;
+<?php
+
+namespace Closca\Sonus;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Response as IlluminateResponse;
 
 class SonusServiceProvider extends ServiceProvider {
+    
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /* Config */
+        $this->mergeConfigFrom(__DIR__.'/../config/sonus.php', 'sonus'); 
+        $this->publishes([
+            __DIR__.'/../config/sonus.php' => config_path('sonus.php'),'config'
+        ]);
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		/* Config */
-		$this->mergeConfigFrom(__DIR__.'/../config/sonus.php', 'sonus'); 
-		$this->publishes([
-		    __DIR__.'/../config/sonus.php' => config_path('sonus.php'),'config'
-		]);
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $app = $this->app;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['sonus'] = $this->app->share(function($app)
-        {
+        // create sonus
+        $app['sonus'] = $app->share(function ($app) {
             return new Sonus;
         });
-	}
+    }
 
 }
